35e6f9f1f771fe61fd10c9c86455949424bbe4f9
br0mabs <steven.lol.hax@gmail.com>
2021-09-18 11:41:37 -0400

    tokenized input and sorted by arguments and flags

--- command.py
+++ command.py
@@ -1,5 +1,11 @@
 import discord
 import config
+import parse_args
 
 async def process(msg):
     await msg.channel.send(msg.content[len(config.PREFIX):])
+    args, flags = parse_args.parse(msg.content[len(config.PREFIX):])
+    print("args:")
+    print(args)
+    print("flags")
+    print(flags)
