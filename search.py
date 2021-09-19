9c72bd292c9aeb7fd85bcef28c5737f2e5313f30
br0mabs <steven.lol.hax@gmail.com>
2021-09-18 20:15:07 -0400

    added search command as well as search function using youtube_dl

---
+++ search.py
@@ -0,0 +1,27 @@
+from youtube_dl import YoutubeDL
+from requests import get
+import discord
+async def find_videos(arg, message):
+    ydl_opts = {
+        'noplaylist' : 'True'
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        try:
+            get(arg)
+        except:
+            video = ydl.extract_info(f"ytsearch5:{arg}", download = False)['entries']
+        else:
+            video = ydl.extract_info(arg, download = 'False')
+    ans = ""
+    for i in range(min(5, len(video))):
+        ans += str(i + 1)
+        ans += "."
+        ans += "\n"
+        ans += video[i]['title']
+        ans += "\n"
+        ans += video[i]['webpage_url']
+        ans += "\n"
+    if not ans:
+        await message.channel.send("No results :(")
+    else:
+        await message.channel.send(ans, allowed_mentions = discord.AllowedMentions(everyone = False))
